89a3d3d826a87419682e1e7fb81170af4bb39796
Alaa Abdelbaki <alaa.abdelbaki@esprit.tn>
2021-10-24 11:26:47 +0100

    Added basic game physics and camera movement

---
+++ Assets/Scripts/BallMvt.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMvt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Return))
+        {
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.transform.Translate(-2 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.transform.Translate(2 * Time.deltaTime, 0, 0);
+        }
+    }
+}
